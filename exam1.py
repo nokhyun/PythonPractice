4bbd43f9520fee09f6490beedbe83a59ce4272e8
nokhyun90 <nokhyun90@gmail.com>
2023-03-05 20:49:34 +0900

    선택정렬 측정비교

--- exam1.py
+++ exam1.py
@@ -1,8 +1,34 @@
-array = [3, 5, 1, 2, 4]
-summary = 0
+import time
+from random import randint
 
-for x in array:
-    summary += x
+array = []
+for _ in range(10000):
+    array.append(randint(1, 100))
 
-print(summary)
+start_time = time.time()
 
+for i in range(len(array)):
+    min_index = i
+    for j in range(i + 1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i]
+
+end_time = time.time()
+print("선택 정렬 성능 측정:", end_time - start_time)
+
+####
+array = []
+for _ in range(10000):
+    array.append(randint(1, 100))
+
+start_time = time.time()
+
+array.sort()
+
+end_time = time.time()
+print("기본 정렬 라이브러리 성능 측정:", end_time - start_time)
+
+###### m1 pro 기준 #######
+# 선택 정렬 성능 측정: 3.3009140491485596
+# 기본 정렬 라이브러리 성능 측정: 0.0007081031799316406
